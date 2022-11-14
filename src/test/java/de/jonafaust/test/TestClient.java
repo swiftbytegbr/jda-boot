555529d6ed91129b1750dbb48523db84c83991a1
Hufeisen <hufeisen@hufeisen-games.de>
2022-11-14 18:16:44 +0100

    Hotfix: Added allowed Gateway Intents

--- src/test/java/de/jonafaust/test/TestClient.java
+++ src/test/java/de/jonafaust/test/TestClient.java
@@ -1,11 +1,15 @@
 package de.jonafaust.test;
 
 import de.jonafaust.jdaboot.JDABoot;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class TestClient {
 
+    public static final String NEPTUNVERSION = "1.0.0";
+
     public static void main(String[] args) {
-        JDABoot.run(TestClient.class);
+        JDABoot.run(TestClient.class, GatewayIntent.values());
+        JDABoot.getInstance().getJda().getGatewayIntents().forEach(System.out::println);
     }
 
 }
--- src/test/java/de/jonafaust/test/TestEvent.java
+++ src/test/java/de/jonafaust/test/TestEvent.java
@@ -3,7 +3,7 @@ package de.jonafaust.test;
 import de.jonafaust.jdaboot.annotation.EventHandler;
 import de.jonafaust.jdaboot.annotation.SetTranslator;
 import de.jonafaust.jdaboot.variables.Translator;
-import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.interactions.DiscordLocale;
 
 public class TestEvent {
@@ -12,7 +12,7 @@ public class TestEvent {
     public static Translator translator;
 
     @EventHandler
-    public void onReady(ReadyEvent event) {
+    public void onReady(GuildMemberJoinEvent event) {
         System.out.println(translator.getTranslatedString(DiscordLocale.GERMAN, "test"));
     }
 
