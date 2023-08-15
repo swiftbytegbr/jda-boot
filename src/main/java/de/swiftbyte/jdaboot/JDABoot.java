6dfc0cd1c0ee2252c246340b44b4a44ba4de7b9f
Hufeisen <hufeisen@hufeisen-games.de>
2023-08-15 20:34:54 +0200

    doc: added to Command and main

--- src/main/java/de/swiftbyte/jdaboot/JDABoot.java
+++ src/main/java/de/swiftbyte/jdaboot/JDABoot.java
@@ -13,7 +13,9 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.exceptions.InvalidTokenException;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
@@ -21,6 +23,11 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import java.util.List;
 import java.util.function.Supplier;
 
+/**
+ * This class is for initializing JDA Boot and starting the Discord bot.
+ *
+ * @since alpha.1
+ */
 @Slf4j
 public class JDABoot {
 
@@ -67,16 +74,38 @@ public class JDABoot {
         init(properties.memberCachePolicy, properties.intents);
     }
 
+    /**
+     * This method starts the Discord bot with the specified information.
+     *
+     * @since alpha.3.5
+     * @param settings the {@link Settings} object to use
+     */
     public static void run(JDABoot.Settings settings) {
         new JDABoot(settings);
     }
 
+    /**
+     * This method starts the Discord bot with the specified information.
+     * For advanced configuration see {@link #run(Settings)}.
+     *
+     * @since alpha.1
+     * @param mainClass the main class of your project
+     * @param memberCachePolicy the {@link MemberCachePolicy} to use
+     * @param allow the list of allowed {@link GatewayIntent} to use
+     */
     public static void run(Class<?> mainClass, MemberCachePolicy memberCachePolicy, GatewayIntent... allow) {
         new JDABoot(mainClass, memberCachePolicy, List.of(allow), ResourceTranslationBundle::new);
     }
 
     /**
-     * @deprecated Use run(JDABoot.Settings) instead
+     * This method starts the Discord bot with the specified information.
+     *
+     * @since alpha.1
+     * @param mainClass the main class of your project
+     * @param memberCachePolicy the {@link MemberCachePolicy} to use
+     * @param translationProvider the custom translation provider to use
+     * @param allow the list of allowed {@link GatewayIntent} to use
+     * @deprecated Use {@link #run(Settings)} instead
      */
     @Deprecated(forRemoval = true)
     public static void run(Class<?> mainClass, MemberCachePolicy memberCachePolicy,
@@ -84,15 +113,29 @@ public class JDABoot {
         new JDABoot(mainClass, memberCachePolicy, List.of(allow), translationProvider);
     }
 
-
+    /**
+     * @see JDA#updateCommands()
+     * @since alpha.2
+     */
     public void updateCommands() {
         jda.updateCommands().queue();
     }
 
-    public void updateCommands(String guildid) {
-        jda.getGuildById(guildid).updateCommands().queue();
+    /**
+     * @see Guild#updateCommands()
+     * @since alpha.2
+     * @param guildId the id of the guild to use
+     */
+    public void updateCommands(String guildId) {
+        jda.getGuildById(guildId).updateCommands().queue();
     }
 
+    /**
+     * @see Guild#upsertCommand(CommandData) 
+     * @since alpha.2
+     * @param guildId the id of the guild to use
+     * @param commandId the jda-boot command id to use
+     */
     public void registerCommand(String guildId, String commandId) {
         jda.getGuildById(guildId).upsertCommand(commandHandler.getCommandData().get(commandId)).queue();
     }
@@ -141,15 +184,38 @@ public class JDABoot {
         jda.awaitReady();
     }
 
+    /**
+     * This class is used for advanced configuration of JDA-Boot.
+     *
+     * @since alpha.3.5
+     */
     @Builder
     public static class Settings {
+
+        /**
+         * The main class of your project.
+         */
         private Class<?> mainClass;
+
+        /**
+         * The {@link MemberCachePolicy} to use.
+         */
         private MemberCachePolicy memberCachePolicy;
+
+        /**
+         * The list of {@link GatewayIntent} to use.
+         */
         private List<GatewayIntent> intents;
 
+        /**
+         * The config provider to use.
+         */
         @Builder.Default
         private Supplier<Config> configProvider = PropertiesConfig::getInstance;
 
+        /**
+         * The translation bundle provider to use.
+         */
         @Builder.Default
         private Supplier<TranslationBundle> translationBundleProvider = ResourceTranslationBundle::new;
     }
--- src/main/java/de/swiftbyte/jdaboot/annotation/command/Command.java
+++ src/main/java/de/swiftbyte/jdaboot/annotation/command/Command.java
@@ -1,37 +1,86 @@
 package de.swiftbyte.jdaboot.annotation.command;
 
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to create a command.
+ * It must belong to a public class that implements the {@link de.swiftbyte.jdaboot.command.SlashCommand},
+ * {@link de.swiftbyte.jdaboot.command.UserContextCommand} or {@link de.swiftbyte.jdaboot.command.MessageContextCommand} interface.
+ *
+ * @since alpha.3.5
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Command {
 
+    /**
+     * The name of the command in discord.
+     */
     String name();
 
+    /**
+     * The description of the command in discord.
+     */
     String description() default "DESCRIPTION MISSING";
 
+    /**
+     * The {@link Type} of the command.
+     */
     Type type();
 
+    /**
+     * The default needed {@link Permission} of the command.
+     */
     Permission enabledFor() default Permission.UNKNOWN;
 
+    /**
+     * True if the command should only be used on servers and not in private messages, otherwise false.
+     */
     boolean guildOnly() default false;
 
+    /**
+     * True if the command should be global, false if it should be added manually to a server.
+     */
     boolean isGlobal() default true;
 
+    /**
+     * The {@link CommandOption}s that the command should have.
+     */
     CommandOption[] options() default {};
 
+    /**
+     * The {@link SubcommandGroup}s that the command should have.
+     */
     SubcommandGroup[] subcommandGroups() default {};
 
+    /**
+     * The {@link Subcommand}s that the command should have.
+     */
     Subcommand[] subcommands() default {};
 
+    /**
+     * The different types of commands discord have.
+     */
     enum Type {
+        /**
+         * A normal slash command.
+         */
         SLASH,
+
+        /**
+         * A command executed on a user context menu
+         */
         USER,
+
+        /**
+         * A command executed on a message context menu
+         */
         MESSAGE
     }
 }
